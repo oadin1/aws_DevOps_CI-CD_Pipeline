ab52fcd97a241a9c7cccf7cbf613b13984043602
oadin <omkar.deungale811@gmail.com>
2025-02-08 21:44:49 +0530

    added new webapplication project

---
+++ src/test/java/com/MarketingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.marketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Test to check if the Spring Boot application context loads successfully
+    }
+
+}
